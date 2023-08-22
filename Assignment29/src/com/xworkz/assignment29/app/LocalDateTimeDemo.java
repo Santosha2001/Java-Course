403596dc892dc5abd8d4d3f2bca9e0c52fe00494
Santosha2001 <santosha.xworkz@gmail.com>
2023-08-22 22:30:39 +0530

    assignment29

---
+++ Assignment29/src/com/xworkz/assignment29/app/LocalDateTimeDemo.java
@@ -0,0 +1,20 @@
+package com.xworkz.assignment29.app;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDemo {
+	public static void main(String[] args) {
+		LocalDateTime dateTime = LocalDateTime.now();
+		System.out.println(dateTime);
+
+		LocalDateTime dateTime2 = LocalDateTime.MAX;
+		System.out.println(dateTime2);
+
+		LocalDateTime dateTime3 = LocalDateTime.MIN;
+		System.out.println(dateTime3);
+
+		LocalDateTime dateTime4 = LocalDateTime.of(2023, 4, 14, 12, 20);
+		System.out.println(dateTime4);
+
+	}
+}
---
+++ Assignment29/src/com/xworkz/assignment29/app/SystemDemo.java
@@ -0,0 +1,20 @@
+package com.xworkz.assignment29.app;
+
+public class SystemDemo {
+	public static void main(String[] args) {
+		
+		System.out.println(System.currentTimeMillis());
+
+		System.out.println(System.clearProperty("Santhu"));
+
+		System.out.println(System.lineSeparator());
+
+		System.out.println(System.nanoTime());
+
+		System.out.println(System.err);
+
+		System.out.println(System.in);
+
+		System.out.println(System.out);
+	}
+}
