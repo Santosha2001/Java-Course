cca9fe2a9384ab28b2b988de202204ef0628107f
Santosha2001 <santosha.xworkz@gmail.com>
2023-10-23 21:22:06 +0530

    exceptins

---
+++ exception/src/com/xworkz/exceptions/StudyException.java
@@ -0,0 +1,7 @@
+package com.xworkz.exceptions;
+
+public class StudyException extends Exception {
+	
+	
+
+}
---
+++ exception/src/com/xworkz/exceptions/WorkRUntimeException.java
@@ -0,0 +1,6 @@
+package com.xworkz.exceptions;
+
+public class WorkRUntimeException extends RuntimeException{
+
+	
+}
